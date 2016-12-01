a709af10c8394a30bfb7a72faf78440b9c7e7239
Ian Brayshaw <ian@denormal.co.uk>
2016-12-01 03:17:54 +0000

    Added package documentation.

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package gitinfo provides an interface to high-level git information for a
+git working copy. gitinfo attempts to use the locally installed
+"git" executable using https://github.com/denormal/go-gittools.
+*/
+package gitinfo
